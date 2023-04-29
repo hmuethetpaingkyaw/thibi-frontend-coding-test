82d6ae8442fd09487fbe9249bd7c057bab077c4c
Hmue Thet Paing Kyaw <94979276+hmuethetpaingkyaw@users.noreply.github.com>
2023-04-30 04:43:49 +0630

    remove unnecessary import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
-import Layout from '@/components/Layout';
 import useGlossary from '@/hooks/useGlossary';
 import { useEffect } from 'react';
-import { Alphabets } from '@/components/Glossary/Alphabets';
 import { GlossaryCard } from '@/components/Glossary/GlossaryCard';
-import useTranslation from 'next-translate/useTranslation';
 import { GlossaryPageLayout } from '@/components/Glossary/GlossaryPageLayout';
 import Loading from '@/components/Loading';
 import { useGlossaryType } from '@/types/Glossary';
